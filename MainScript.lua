ad7b82bd2aeb0b5131ac651a007a5cc5db6a13f8
daydream410 <127705746+daydream410@users.noreply.github.com>
2025-02-20 17:08:25 +0700

    Update MainScript.lua

--- MainScript.lua
+++ MainScript.lua
@@ -6,6 +6,7 @@ function GetGameScript()
     if ID == 2693023319 then  -- Ganti dengan ID game yang sesuai
         return "EAenc.lua"
     else
+        -- print("Game not supported")
         return nil
     end
 end
@@ -14,8 +15,7 @@ local gameScript = GetGameScript()
 
 if gameScript then
     -- Menampilkan notifikasi "Correct Game"
-    local StarterGui = game:GetService("StarterGui")
-    StarterGui:SetCore("SendNotification", {
+    game.StarterGui:SetCore("SendNotification", {
         Title = "Game Check",
         Text = "CORRECT GAME!",
         Duration = 5
@@ -23,21 +23,12 @@ if gameScript then
     task.wait(5)
 
     -- Memuat dan menjalankan skrip dari GitHub
-    local HttpService = game:GetService("HttpService")
     local scriptURL = baseURL .. gameScript
-    local scriptContent = HttpService:GetAsync(scriptURL)
-
-    -- Membuat ModuleScript untuk mengeksekusi kode yang diambil
-    local moduleScript = Instance.new("ModuleScript")
-    moduleScript.Source = scriptContent  -- Menyisipkan kode dari URL
-    moduleScript.Parent = game.ServerScriptService  -- Menempatkannya dalam ServerScriptService
-
-    -- Mengaktifkan ModuleScript
-    local module = require(moduleScript)
+    local scriptContent = game:HttpGet(scriptURL)
+    loadstring(scriptContent)()
 else
     -- Jika game ID tidak cocok, tampilkan notifikasi atau berhenti
-    local StarterGui = game:GetService("StarterGui")
-    StarterGui:SetCore("SendNotification", {
+    game.StarterGui:SetCore("SendNotification", {
         Title = "Game Check",
         Text = "GAME NOT SUPPORTED!",
         Duration = 5
