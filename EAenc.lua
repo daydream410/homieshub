f2ebf8cc3c5681943610f8353daf3cf9d4c7f6c9
daydream410 <127705746+daydream410@users.noreply.github.com>
2025-02-20 17:45:20 +0700

    Create EAenc.lua

---
+++ EAenc.lua
@@ -0,0 +1,32 @@
+~'vZvkx jgFzm?ȫ&z{]hvk'zWByhv
+jgh{.x^zKjuiا++eyBwڶ*'I'i^vxZ%ujghXx֦xz&+tInWjm:"*.8rݱz^rh˩+Hyc
+=,
+>8^+byy'اr[yجr݂āجr݂ ]m4^zh{j[䞶bj[azh{ayجrzئz^Zv+nK+-zkk')8z&nح,鍢׊{"'+RzެXx֦xz&TiZ(zxij^n)ܢokµVж%ƥ1M֊wh*jדiZjStrبƢ6
+rبƢ~'t-^zm'+B{^r"z!ڶ*'䉚wӎם{-"	Zk VrGbww׉jS%zZjX~)n	^1M­y: Wjg׉jEjYCjfx+Unyej'|YZ&rبV1j۫h~)j[zZjy╨j[uⶈ Ɗw;V+&zxȝ׉jii֤jzfZjWb{H׉j\r׫jz"uZ-zh(bhqTȞIǠjgzԞyKԞ{zx!FzI׭zK"b^ޑfZq,z'Rzq'Az
+y_w-➛>zwzf))ۤPjZyj'ǬZاTȞO*^{Rǫ"zn<xnu݊zn
+^{)삡גƊw;^zx؟ȝ׉jaf'By+Z'aԭjדʗjbZL5v)"Ǜz[jXzJ'v+"vnjz"t(Z^{隞J֭y<y'^קuzw~ǧx FjK^j)쉪Z'kRzqk'jSk'Z-ze(Z-z+Zr׫^ufǭ'~)n!jǧx 'uzx"w+^&驦Zr׫^t*'-~ܶ*'r׫nߊxxw
+i^i'⑫""w+^&v&'jvjbb}Vazxj^kIBzwM؟ƭӊ^zm7ߊxx-yb~Qk'zWM؟8D
+*'e
+-YZ}b~묊x׉jCЉ鞲Ơ{*)z[J֫ƺ$*+y't-jب8'{H׉jCy-yb~^קw!(!: Wƥ~VX	^1M­y: Wjg!(!+UnyejejemV	em$~ܶ*'UnzZjZek'>Vzƥ>Vzhq\r׫Vzj讙Zr׫^ufjbǺf'QOjZumngjxFjz"u(Z-zh(bhڮ~ǧx ǥjfubFf'QOjm֤v+^),zK'nhڮazxj^kIBzwM؟ƭӊ^7{jz"u(z-~t;jب޶zw~[ȮۤڙJ-(!ZjWbvu*h+>Vz1雹RKmX'֧q*hbOjjz"u(jjnv+^݈{jz"u(XZjqZɩbA(!w)Zơk&m
+h*%߮ny۞~fbX'i^v{jjnjVmx%{~+ɩ׫~.X-f'QOjBx(W
+څơk&zx"bjjx$jǧjfچFb+h}Jh+"ZjZ~ǭ"{-jw�֜g߱Ojjz"u(߱OȭyhyMjǝHv&j-zb@֜gJ~ǭzw~-jȬim%yؚ؟ڵz'jKb~FZ筅jgz'
+I6b~'*'N+exz&ejemb^t;jب1ڙjjn b&jFiRKm.k'z[J֫ƺ$*+y't-jب8'{E!(!7rدjםڶ*'zx!z{bҦem bbƥ{).(!jz"u(XtXСWN|)Zĩb}bاazwbDyږǾǦjؤjyZ+-xm
+-ǧZjzvzw^קwjYJXzhqR'^Ƣ6
+䥉׫5YdJuwB'rz{uJߋyBޞZYZbu&rبVk'jSk'
+i^{jz"uf*^yZ瞝߾&>)buƥJXztl*jג'^֦x0zhe
+Zߝޙ+~,O
+z{Uj[סej%׫VrGbuZjgqO(Z-znj[zw~jھ+rJXzhqR'^Ƣ6
+䥉׫5܍V6jjxם4"w+zgT}܂ޞZjg,܅,'^
+ھ+r	em$~ܶ*'Unzyj+ZqjVwI-ۤ~+nV|azrبƢ6
+rبazhqEx&z%xƢ6
+	^5
+z{Uj[}lxYZZ{7jghYZ&rبVhqixZ.Zƥq娮K)iǂޞКn"{ngm.zjy~ږrبǭzvzvaz1雹djgj؝jFޮjFjޮjqڢ&jجqbzWN}Xý>ǧnS╨j	'`O*^I֛1蝩-ڑ趉+$jgiƦzyƦz{zxځh+"u֬׭j-i袛&zxځjڵZZn)ii֩Ȭ֧֬&ƦjZjWb{u^	!Wy>ǧZڙj^ljfzf)Fz֤mzqq_1zڮ詢ȬFzzؚM]zؤzw^߾2ZZ(I-F^l+b+ju^	6b~)+)zfy+Z׫zШ䞝h'qbtWx&z{Bjgjj/zg+"׫myb~^ǾǧjXjyڑ^uں[\jgh&zʗjgi<x+ǧ-Gmbzjz-fzw^߾1jKb~Fj֮蝩-{7jgjǭUnybbS%zAbRzk>VzhqaemN tl*jד%x֦xxX%ZºޞZj[V(!(!YZ&rبVhqi឵''>Vz>[Ȯۤڙemrب](!(!Z~Z镬qOݦVzjxu-v*݊Kjjek'颖)"隞F-=uyeƥri^Zr׫!jڝ!jŊwE-
+t{jz"u(ri^XtXСWGymǧjfچF(!(!8ifjZjWbb\r׫)*(bӟ
+VxX؟^+-xemX'֧qxXbbOjZ-zbbEYBZ+
+hwdFny۞t嚮vxXӮXx*%~&D`v睹ny嚮vء(!(!7(u'abCza2^\çNaem)eNzw2ӟNj{"ȡem:ew쥪ޝ̴j{)j*.Xyjvjj)jzZyǤjxڭ%ƥ-ޕZ-zbbB)]9jY[~z--ޖ^zvmdjyZmgjfFz[i֧jgi"{-jwY[jgi+眅r׫杝-ޕ(y@iMtM-ޕ+nvß~ǭU-{
+7vxzP%:tzZj[^mHZǧ{mm{Ki=ޞڙmm{KiJ,P8wMtm-ޔ
+wS쥪ޝ̵m-ޕ7Zjg{KiMm
+hp~&D`v睹nym-ޕ7+kG쥪ޝ̴ױzTԢx-ޔz{\r׫zw^קun"{ngbbeƥ~ܶ*'騽bi'镬(Z-zazyhq^+-xemVzjŊwE-
+t,(!(!'+-xem{)(!(!72zw~ǧZmgjfƥ-ޖZr׫)*(bӟ
+VѮ'jgi-ޔ72zw^קwjY[i$zؤj W-'j[J֫ƺ$*+y't-jب8'{OxX+ڵCjXjj)ib'ek'zZ)ZvtxXek'zw~ǧjfچF(!(!J^ɩmiǭzV bjKb~FVz]yШw](!(!lzy+Z׫zШ䞝h'qbtWh{Mm>VzbCy-yb~~ǧbb]jzk隊{jh	^v))+Z➖ek'g>VzemVzuzw~ߡ6{nZyjZjR'~mhvy6)~ǧjfچFޖIY)ߢ*^֧ZjX~mHiy^-ޔz&"z!᚞ؘjgzxvjn歖*'-ޔߡ6
+mUz"}t᚞صקz"xn,Țk*.8j~mOȭ)4M䞲梑ȬVzZ^֦j*'-ޔ,ب@iM4OI.j),ȥin
